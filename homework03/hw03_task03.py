dade8daf1e3b1657e76c088e771ed0a6693e2184
sergey13prokop <sergey13.prokop@gmail.com>
2022-01-30 22:18:38 +0200

    hw03 05/10task

--- homework03/hw03_task03.py
+++ homework03/hw03_task03.py
@@ -12,8 +12,13 @@
 # В задании глубина шаблона = 5 (максимум звездочек на пике)
 depth = int(input("\nВведите глубину шаблона: "))
 
+print("\n\33[3mС ВЛОЖЕННЫМ ЦИКЛОМ")
 for i in range(1, depth * 2):
     printStr = ""
     for _ in range(depth - abs(depth - i)):
         printStr += "*"
     print("\33[34m" + printStr)
+
+print("\n\33[0m\33[3mБЕЗ ВЛОЖЕННОГО ЦИКЛА")
+for i in range(1, depth * 2):
+    print("\33[34m*"*(depth - abs(depth - i)))
