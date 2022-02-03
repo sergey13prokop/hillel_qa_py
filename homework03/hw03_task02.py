c13ef7795ec16eae174399244417c1010a69b1e3
sergey13prokop <sergey13.prokop@gmail.com>
2022-02-03 17:01:49 +0200

    hw02 02/10task (range -1)

--- homework03/hw03_task02.py
+++ homework03/hw03_task02.py
@@ -4,7 +4,7 @@
 inputStr = input("\nВведите строку: ")
 outputStr = ""
 
-for i in range(len(inputStr)):
-    outputStr += inputStr[len(inputStr)-i-1]
+for i in range(len(inputStr), 0, -1):
+    outputStr += inputStr[i - 1]
 
 print("Зеркало строки: \33[34m\33[3m" + outputStr)
