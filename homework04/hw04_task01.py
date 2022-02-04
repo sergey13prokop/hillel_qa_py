700c98ddcbbcde0e2fd15e5582a60dc19177df52
sergey13prokop <sergey13.prokop@gmail.com>
2022-02-04 16:08:04 +0200

    hw04 01/09

--- homework04/hw04_task01.py
+++ homework04/hw04_task01.py
@@ -1,3 +1,10 @@
-# Напишите программу для преобразования списка в кортеж.
+# Напишите программу для преобразования списка в кортеж
 
+import random
+LIST_QTY = 10
 
+l = [random.randint(0,9) for _ in range(0,LIST_QTY)]
+print(f"\n{type(l)} : ", l)
+
+t = tuple(l)
+print(f"{type(t)}: \33[34m", t)
