9be4949bf2745a758dbfa4113807767eaefa2a0f
sergey13prokop <sergey13.prokop@gmail.com>
2022-02-07 17:13:09 +0200

    hw04 08/09

--- homework04/hw04_task08.py
+++ homework04/hw04_task08.py
@@ -1,18 +1,23 @@
-# Реализовать логику Union для двух списков (list),
+# Реализовать логику set.Union для двух списков (list),
 # проверить работу алгоритма на set.union
 
-list1 = list(input("\nВведите строку для преобразования в список 1: "))
-list2 = list(input("Введите строку для преобразования в список 2: "))
+list1 = list(input("\nВведите строку для преобразования в список и множество 1: "))
+list2 = list(input("Введите строку для преобразования в список и множество 2: "))
+set1 = set(list1)
+set2 = set(list2)
 
 print("\nСписок 1:", list1)
 print("Список 2:", list2)
+print("\nМножество 1:", set1)
+print("Множество 2:", set2)
 
 list12 = list()
 for i in range(0, len(list1)):
-    list12.append(list1[i])
+    if list1[i] not in list12:
+        list12.append(list1[i])
 for i in range(0, len(list2)):
-    list12.append(list2[i])
+    if list2[i] not in list12:
+        list12.append(list2[i])
 
-print("\n\33[34mВручную union:", list12)
-print("\33[35mlist1 + list2:", list1 + list2)
-print("\33[31mset.union:", set.union(set(list1), set(list2)))
+print("\n\33[34munion.list:", list12)
+print("\33[31mset.union :", set.union(set1, set2))
